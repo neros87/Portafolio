209f72d132788c481fe526985fc33c693fad375b
neros <neros.anm@gmail.com>
2017-09-27 10:01:07 -0700

    Servicio de Firebase Listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,13 +6,29 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargada_sobre_nosotros = true;
+  equipo:any[] = [];
 
   constructor( public http:Http ) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info() {
     this.http.get("assets/data/info.pagina.json")
+      .subscribe( data => {
+        // console.log(data.json());
+        this.cargada = true;
+        this.info = data.json();
+      })
+  }
+
+  carga_sobre_nosotros() {
+    this.http.get("https://angularwebpage.firebaseio.com/equipo.json")
       .subscribe( data => {
         console.log(data.json());
         this.cargada = true;
-        this.info = data.json();
+        this.equipo = data.json();
       })
   }
 }
